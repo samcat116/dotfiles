c778c2ac9d86334a62cdaf038f358f179970603d
Sam Schmitt <sam@samschmitt.com>
2021-12-09 22:09:32 -0500

    duh

--- run_once_before_install-change-shell.sh
+++ run_once_before_install-change-shell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ ! -d ~/.zshrc ] ; then
+if [ ! -f ~/.zshrc ] ; then
 	echo "ZSH not found, installing..."
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
