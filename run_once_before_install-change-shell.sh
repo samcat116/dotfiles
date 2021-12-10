e42dab3c6a73de671c2376db1154e0d0da24aa28
Sam Schmitt <sam@samschmitt.com>
2021-12-09 22:51:42 -0500

    Install p10k

--- run_once_before_install-change-shell.sh
+++ run_once_before_install-change-shell.sh
@@ -10,4 +10,8 @@ if [ ! -f ~/.zshrc ] ; then
 	chsh -s $(which zsh)
 	echo "Installing Oh-My-Zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "Installing PowerLevel10k"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	p10k configure
 fi
