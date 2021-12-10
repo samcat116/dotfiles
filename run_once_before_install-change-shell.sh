f4b84e377d26c253648677b47aac2825217cb65e
Sam Schmitt <sam@samschmitt.com>
2021-12-09 22:20:20 -0500

    Install zsh based on distro

--- run_once_before_install-change-shell.sh
+++ run_once_before_install-change-shell.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 if [ ! -f ~/.zshrc ] ; then
 	echo "ZSH not found, installing..."
+	if [ -f /etc/lsb-release ] ; then
+		sudo apt install zsh
+	elif [ -f /etc/redhat-release ]; then
+		sudo yum install zsh -y
+	fi
+	echo "Changing default shell for $USER"
+	chsh -s $(which zsh)
+	echo "Installing Oh-My-Zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
