9983b9aac45a59469d8a55aef2c99ddbe686f1c1
Sam Schmitt <sam@samschmitt.com>
2021-12-25 16:24:18 -0500

    Added p10k config

--- run_once_before_install-change-shell.sh
+++ run_once_before_install-change-shell.sh
@@ -4,7 +4,7 @@ if [ ! -f ~/.zshrc ] ; then
 	if [ -f /etc/lsb-release ] ; then
 		sudo apt install zsh -y
 	elif [ -f /etc/redhat-release ]; then
-		sudo yum install zsh git -y
+		sudo yum install zsh git util-linux-use-y
 	fi
 	echo "Changing default shell for $USER"
 	chsh -s $(which zsh)
@@ -14,4 +14,5 @@ if [ ! -f ~/.zshrc ] ; then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 	source ~/.zshrc
+	source ~/.p10k.zsh
 fi
